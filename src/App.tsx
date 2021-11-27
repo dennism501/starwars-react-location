9a25d7cf7155d4a64ba1a6e4c333c4b87bdd521d
Dennis Mubamba <dennism501@outlook.com>
2021-11-27 12:31:46 +0200

    :construction: Added react-query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from "react-query/devtools";
+import {Router, Outlet, ReactLocation} from 'react-location'
 
-
+const queryClient = new QueryClient()
+const location = new ReactLocation()
 function App() {
   return (
-      <>
-      </>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
